4beb720b6a9902df566aa1a8ff24d5717ccb388a
hsinming <hsinming.chen@gmail.com>
2018-01-20 17:30:25 +0800

    bug fix

---
+++ image_normalization.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import random
+import numpy as np
+import scipy.misc
+import logging
+
+#https://docs.python.org/3/howto/logging-cookbook.html
+def get_logger(ch_log_level=logging.ERROR, 
+               fh_log_level=logging.INFO):
+    logging.shutdown()
+    imp.reload(logging)
+    logger = logging.getLogger("CheXNet")
+    logger.setLevel(logging.DEBUG)
+    
+    # Console Handler
+    if ch_log_level:
+        ch = logging.StreamHandler()
+        ch.setLevel(ch_log_level)
+        ch.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(ch)
+    
+    # File Handler
+    if fh_log_level:
+        fh = logging.FileHandler('CheXNet.log')
+        fh.setLevel(fh_log_level)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+def get_paths_to_files(dir_path):
+    filepaths = []
+    fnames = []
+    for (dirpath, dirnames, filenames) in os.walk(dir_path):
+        filepaths.extend(os.path.join(dirpath, f) for f in filenames if not f[0] == '.')
+        fnames.extend([f for f in filenames if not f[0] == '.'])
+    return filepaths, fnames
+
+def get_random_image_path(dir_path):
+    filepaths = get_paths_to_files(dir_path)[0]
+    return filepaths[random.randrange(len(filepaths))]
+
+def load_img_as_np_arr(img_path):
+    return scipy.misc.imread(name=img_path, mode='RGB')
+
+def get_mean_std_of_dataset(dir_path, sample_size=5):
+    fpaths, fnames = get_paths_to_files(dir_path)
+    random.shuffle(fpaths)
+    total_mean = np.array([0.,0.,0.])
+    total_std = np.array([0.,0.,0.])
+    for f in fpaths[:sample_size]:
+        img_arr = load_img_as_np_arr(f)
+        mean = np.mean(img_arr, axis=(0,1))
+        std = np.std(img_arr, axis=(0,1))
+        total_mean += mean
+        total_std += std
+    avg_mean = total_mean / sample_size
+    avg_std = total_std / sample_size
+    print("mean: {}".format(avg_mean), "stdev: {}".format(avg_std))
+    return avg_mean, avg_std
+
+def main():
+    CXR_images = '/data/CXR8/images'
+    mean, std = get_mean_std_of_dataset(CXR_images, 100)
+    print('mean={mean}, std={std}'.format(mean=mean, std=std))
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
